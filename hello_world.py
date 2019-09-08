6953fbe83b2e955dfb01808b44c9d37325932d0f
Austin Wentworth <asw0019@knights.ucf.edu>
2019-09-08 18:09:27 -0400

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+print("Hello World");
+print ("You can love someone deep inside your heart and there is nothing wrong with it. If a lot of people loved each other, the world would be a better place to live.");
